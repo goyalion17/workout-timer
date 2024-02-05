9f9fcc704b595817bf39f48b9c42c3d01a802e5e
Irina Saveleva <goyalion17@gmail.com>
2024-02-05 20:22:40 +0100

    closures in Effects

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,15 +8,6 @@ function Calculator({ workouts, allowSound }) {
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration, setDuration] = useState(0);
 
-  // const playSound = useCallback(
-  //   function () {
-  //     if (!allowSound) return;
-  //     const sound = new Audio(clickSound);
-  //     sound.play();
-  //   },
-  //   [allowSound]
-  // );
-
   useEffect(
     function () {
       setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
@@ -34,6 +25,13 @@ function Calculator({ workouts, allowSound }) {
       playSound();
     },
     [duration, allowSound]
+  );
+
+  useEffect(
+    function () {
+      document.title = `Your ${number}-exercise workout`;
+    },
+    [number]
   );
 
   const mins = Math.floor(duration);
